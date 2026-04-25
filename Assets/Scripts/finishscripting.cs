76b2b09493b745268b34d9be453cd46fee5e2f16
YigitcanCaliskan <yigit.can.isik52@gmail.com>
2026-04-26 01:39:59 +0300

    I did't find last"!"

--- Assets/Scripts/finishscripting.cs
+++ Assets/Scripts/finishscripting.cs
@@ -1,19 +1,30 @@
+using System;
+using BloomJam;
 using BloomJam.Player;
 using UnityEngine;
+using YigitcanCaliskan;
 using YigitcanCaliskan.EventBus;
+using YigitcanCaliskan.ServiceLocator;
 
 public class finishscripting : MonoBehaviour
 { 
     private void OnEnable()
     {
+        ServiceLocator.Get<IInputService>().OnReload += downlo;
         EventBus.Subscribe<PlayerDiedEvent>(debugfinish);
     }
 
     private void OnDisable()
-    {
+    {        ServiceLocator.Get<IInputService>().OnReload -= downlo;
+
         EventBus.Unsubscribe<PlayerDiedEvent>(debugfinish);
     }
 
+    public void downlo()
+    {
+        ServiceLocator.Get<ISceneService>().LoadMainMenu();
+    }
+  
 
     private void debugfinish(PlayerDiedEvent evt)
     {
